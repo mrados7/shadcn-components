83214c51eb306ddf34f8e0a6a74bb382cb98ac2e
Marko Rados <marko.rados7@gmail.com>
2024-09-20 23:02:58 +0200

    fix tailwind not loading

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/main";
+import React from "react";
+import "./index.css"
+
+interface IProps {}
+
+const App: React.FC<IProps> = () => {
+  return <div>
+    <Button variant="default">Ivafn</Button>
+  </div>;
+};
+
+export default App;
