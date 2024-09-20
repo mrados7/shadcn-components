f8c06e8b69b7c8d94bc85f909fbe1fa887c1d58f
Marko Rados <marko.rados7@gmail.com>
2024-09-20 23:02:58 +0200

    fix tailwind not loading

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,2 @@
-export function helloAnything(thing: string): string {
-  return `Hello ${thing}!`
-}
-
 export { Button, buttonVariants } from "./components/ui/button"
+export { cn } from "./lib/utils"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ["class"],
-    content: [],
+    content: [
+			'./src/**/*.{ts,tsx}',
+		],
   theme: {
   	extend: {
   		borderRadius: {
